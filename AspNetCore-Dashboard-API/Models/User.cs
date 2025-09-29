8e055649fa47475e23520387b382d25febbaf289
Baraa <engbaraakhamaysa@gmail.com>
2025-09-29 23:58:45 +0300

    Update Model User

--- AspNetCore-Dashboard-API/Models/User.cs
+++ AspNetCore-Dashboard-API/Models/User.cs
@@ -5,11 +5,17 @@ namespace AspNetCore_Dashboard_API.Models
 {
     public class User
     {
-        [BsonId]
+        [BsonId] 
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
 
-        [BsonElement("Name")]
+        [BsonElement("Name")] 
         public string Name { get; set; }
+
+        [BsonElement("Email")] 
+        public string Email { get; set; }
+
+        [BsonElement("Password")] 
+        public string Password { get; set; }
     }
 }
